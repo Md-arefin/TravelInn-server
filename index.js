0aadf89a8bc923ec34a609eb830753aafe76c2e0
Md-arefin <ansariarefin617@gmail.com>
2023-10-17 01:34:01 +0600

    implemented all users

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000;
 
 const uri = `mongodb+srv://${process.env.USER_Name}:${process.env.PASSWORD}@cluster0.bqstehg.mongodb.net/?retryWrites=true&w=majority`;
@@ -15,6 +15,26 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("Travlling server is running...")
 });
+
+// Verify JWT
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        return res.status(401).send({ error: "Unauthorized access!" });
+    }
+
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: "Unauthorized access!" });
+        }
+
+        req.decoded = decoded;
+        next();
+    });
+};
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,9 +52,99 @@ async function run() {
         await client.connect();
 
         // Collections
-        const usersCollection = client.db("travellig").collection("users");
+        const usersCollection = client.db("travelling").collection("users");
 
         // user related API
+        app.get("/all-users", async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ error: "Forbidden access" });
+            // }
+            const query = { email: email };
+            const result = await usersCollection.findOne(query);
+
+            res.send(result);
+        });
+
+        app.get("/users/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ error: "Forbidden access" });
+            // }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === "admin" }
+            res.send(result);
+        });
+
+        app.post("/add-users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: "User already exist" });
+            }
+            // console.log("user", user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // review related API
+        app.get("/get-review", async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/add-review", verifyJWT, async (req, res) => {
+            const email = req.body.email;
+            const review = req.body;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: "forbidden access" });
+            }
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        });
+
+        // JWT related api
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "10h",
+            });
+            res.send({ token });
+        });
 
 
 
